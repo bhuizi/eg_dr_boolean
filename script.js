f1af8818af38b213d369000d228b2010d1cab0b8
bryan huizi <bryanhuizi@gmail.com>
2019-01-20 09:38:02 -0800

    04: base w/map

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 
 const Right = x => ({
+  chain: f => f(x),
   map: f => Right(f(x)),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`
@@ -13,19 +14,32 @@ const Left = x => ({
   inpspect: () => `Left(${x})`
 });
 
-
+const tryCatch = f => {
+  try {
+    return Right(f())
+  }catch(e){
+    return Left(e)
+  }
+}
 const fromNullable = x =>
   x != null ? Right(x) : Left(null)
 
-const getPort = () => {
-  try {
-    const str = fs.readFileSync('./cofig.json')
-    const config = JSON.parse(str)
-    return config.port
-  } catch(e) {
-    return 3000
-  }
-}
+// const getPort = () => {
+//   try {
+//     const str = fs.readFileSync('./config.json')
+//     const config = JSON.parse(str)
+//     return config.port
+//   } catch(e) {
+//     return 3000
+//   }
+// }
+
+const getPort = () =>
+  tryCatch(() => fs.readFileSync('./config.json'))
+    .map(c => JSON.parse(c))
+    .fold(e => 3000,
+        c => c.port
+      )
 
 const result = getPort()
 console.log(result);
